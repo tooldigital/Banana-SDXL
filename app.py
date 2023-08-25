5319973fb212a28c091ced71599021378944ba59
wimvanhenden-tool <wim.vanhenden@toolofna.com>
2023-08-25 15:55:27 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from potassium import Potassium, Request, Response
+from diffusers import DiffusionPipeline
+import torch
+import base64
+from io import BytesIO
+app = Potassium("my_app")
+
+# @app.init runs at startup, and loads models into the app's context
+@app.init
+def init():
+
+    base = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=False)
+    base.to("cuda")
+
+    refiner = DiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-refiner-1.0",
+        text_encoder_2=base.text_encoder_2,
+        vae=base.vae,
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+        variant="fp16",
+    )
+    refiner.to("cuda")
+
+    context = {
+        "model": base,
+        "refiner": refiner,
+    }
+
+    return context
+
+# @app.handler runs for every call
+@app.handler("/")
+def handler(context: dict, request: Request) -> Response:
+    model = context.get("model")
+    refiner = context.get("refiner")
+    prompt = request.json.get("prompt")
+   
+    n_steps = 40
+    high_noise_frac = 0.8
+
+    image = model(
+        prompt=prompt,
+        num_inference_steps=n_steps,
+        denoising_end=high_noise_frac,
+        output_type="latent",
+    ).images
+
+    image = refiner(
+        prompt=prompt,
+        num_inference_steps=n_steps,
+        denoising_start=high_noise_frac,
+    image=image,
+    ).images[0]
+
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG", quality=80)
+    img_str = base64.b64encode(buffered.getvalue())
+
+    # You could also consider writing this image to S3
+    # and returning the S3 URL instead of the image data
+    # for a slightly faster response time
+
+    return Response(
+        json = {"output": str(img_str, "utf-8")}, 
+        status=200
+    )
+
+if __name__ == "__main__":
+    app.serve()
---
+++ download.py
@@ -0,0 +1,12 @@
+# This file runs during container build time to get model weights built into the container
+
+from diffusers import DiffusionPipeline
+import torch
+
+def download_model():
+    # do a dry run of loading the huggingface model, which will download weights
+    base = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=False)
+    refiner = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-refiner-1.0",text_encoder_2=base.text_encoder_2,vae=base.vae,torch_dtype=torch.float16,use_safetensors=True,variant="fp16",)
+
+if __name__ == "__main__":
+    download_model()
