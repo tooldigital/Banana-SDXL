4af8ddb366165c8291cd38e7d1b8c3c3cc55cdbb
wimvanhenden-tool <125361595+wimvanhenden-tool@users.noreply.github.com>
2023-08-29 19:07:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def handler(context: dict, request: Request) -> Response:
     image = model(prompt=prompt).images[0]
     
     buffered = BytesIO()
-    image.save(buffered, format="JPG",quality=80)
+    image.save(buffered, format='JPEG',quality=80)
     img_str = base64.b64encode(buffered.getvalue())
 
     # You could also consider writing this image to S3
